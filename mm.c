f7adbf22ecdb5a64817f86001cc14964e0e7db7e
Ryan Farr <ryanfarr01@gmail.com>
2015-11-18 11:58:01 -0700

    Beginning of linked list implementation
    It's totally broken

--- mm.c
+++ mm.c
@@ -41,7 +41,6 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 #define WSIZE 4
@@ -65,24 +64,46 @@ team_t team = {
 #define NEXT_BLKP(bp) ((char *) (bp) + GET_SIZE(((char *) (bp) - WSIZE)))
 #define PREV_BLKP(bp) ((char *) (bp) - GET_SIZE(((char *) (bp) - DSIZE)))
 
-void* heap_listp;
+
+//Our macros
+#define GET_SIZE_SIZEP(p) (*(uint *)p)
+#define GET_NEXT(p) ((uint *)(p + 4))
+
+void* head;
+void* next;
+void* heap_listp; //DELETE
 
 int mm_check(void)
 {
     return 0;
 }
 
+//Returns pointer before the one we need or head or null
 static void *find_fit(size_t asize)
 {
-    void *bp;
+    void *p = head;
 
-    for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-        if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
-            return bp;
-        }
+    if(GET_SIZE_SIZEP(p) >= asize) { return p; } //case: head
+
+    while(1)
+    {
+        if(*GET_NEXT(p) == -1) { break; }
+        if(GET_SIZE_SIZEP(GET_NEXT(p)) >= asize) { return p; } //case: pointer before one we need
+
+        p = GET_NEXT(p);
     }
 
     return NULL;
+
+    // void *bp;
+
+    // for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+    //     if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+    //         return bp;
+    //     }
+    // }
+
+    // return NULL;
 }
 
 static void place(void *bp, size_t asize)
@@ -147,23 +168,62 @@ static void *extend_heap(size_t words)
     return coalesce(bp);
 }
 
+
+void *PlaceInfo(void *prev, void *current, size_t neededSize)
+{
+    printf("In PlaceInfo \n");
+    size_t currentSize = GET_SIZE_SIZEP(current);
+
+    if(currentSize >= neededSize + 8)
+    {
+        void* next = current + 4 + neededSize;
+
+        PUT(prev + 4, (uint)next);
+        
+        PUT(next, currentSize - neededSize - 4);
+        PUT(next + 4, (uint)GET_NEXT(current));
+
+        printf("placing at address: %p with size: %d\n and splitting", current, neededSize);
+        return current + 4;
+    }
+    else
+    {
+        PUT(prev + 4, (uint)GET_NEXT(current));
+
+        printf("placing at address: %p with size: %d\n", current, neededSize);
+        return current + 4;
+    }
+}
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
-        return -1;
+    head = mem_sbrk(4*WSIZE) + 4;
 
-    PUT(heap_listp, 0);
-    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
-    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
-    PUT(heap_listp + (3*WSIZE), PACK(0, 1));
-    heap_listp += (2*WSIZE);
+    uint size = 4*WSIZE;
 
-    //extend the heap
-    if(extend_heap(CHUNKSIZE/WSIZE) == NULL)
-        return -1;
+    printf("start is: %p\n", head);
+    printf("size is: %d\n", size);
+
+    PUT(head, size-4);
+    PUT(head + 4, -1);
+    printf("working\n");
+    printf("size based on head: %d\n", (*(uint*)head));
+
+    // if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+    //     return -1;
+
+    // PUT(heap_listp, 0);
+    // PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+    // PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+    // PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+    // heap_listp += (2*WSIZE);
+
+    // //extend the heap
+    // if(extend_heap(CHUNKSIZE/WSIZE), == NULL)
+    //     return -1;
 
     return 0;
 }
@@ -174,38 +234,104 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
- //    int newsize = ALIGN(size + SIZE_T_SIZE);
- //    void *p = mem_sbrk(newsize);
- //    if (p == (void *)-1)
-    // return NULL;
- //    else {
- //        *(size_t *)p = size;
- //        return (void *)((char *)p + SIZE_T_SIZE);
- //    }
+    printf("Beginning malloc: \n");
+    size_t usableSize = ALIGN(size);
 
-    size_t asize;
-    size_t extendsize;
-    char *bp;
+    void *p = head; //find_fit(usableSize);
 
-    if(size == 0) { return NULL; }
+    uint head_size = GET_SIZE_SIZEP(head);
+    if(head_size >= usableSize)
+    {
+        printf("fits in head\n");
+        if(head_size >= usableSize + 8)
+        {
+            printf("splitting head\n");
+            head = head + 4 + usableSize;
+            PUT(head, head_size - usableSize - 4);
+            PUT(head + 4, (uint)GET_NEXT(p));
 
-    if(size <= DSIZE)
-        asize = 2*DSIZE;
+            printf("success in malloc\n");
+            return p + 4;
+        }
+        else
+        {
+            printf("Not splitting head");
+            if(*GET_NEXT(head) != -1)
+            {
+                head = GET_NEXT(head);
+                printf("success in malloc\n");
+
+                return p + 4;
+            }
+            else
+            {
+                //expand
+                printf("Expanding\n");
+                size_t extendSize = MAX(usableSize, CHUNKSIZE);
+                void *current;
+                if((current = mem_sbrk(extendSize) + 4) == NULL)
+                    return NULL;
+
+                printf("FInishing with PlaceInfo\n");
+                return PlaceInfo(head, current, usableSize);
+            }
+        }
+    }
     else
-        asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+    {
+        printf("Head doesn't fit, going to next\n");
+        while(*GET_NEXT(p) != -1)
+        {
+            uint next_size = GET_SIZE_SIZEP(GET_NEXT(p));
+            if(next_size >= usableSize)
+            {
+                printf("Found a fit\n");
+                void *temp = GET_NEXT(p);
 
-    if((bp = find_fit(asize)) != NULL) {
-        place(bp, asize);
-        return bp;
+                printf("Finishing with PlaceInfo\n");
+                return PlaceInfo(p, temp, usableSize);
+            }
+
+            p = GET_NEXT(p);
+        }
+
+        printf("Nothing fit\n");
+        //expand
+
+        printf("Expanding\n");
+        size_t extendSize = MAX(usableSize, CHUNKSIZE);
+        void *current;
+        if((current = mem_sbrk(extendSize) + 4) == NULL)
+            return NULL;
+
+        printf("Finishing with PlaceInfo\n");
+        return PlaceInfo(p, current, usableSize);
     }
 
-    extendsize = MAX(asize, CHUNKSIZE);
-    if((bp = extend_heap(extendsize/WSIZE)) == NULL)
-        return NULL;
+    // size_t asize;
+    // size_t extendsize;
+    // char *bp;
 
-    place(bp, asize);
-    return bp;
+    // if(size == 0) { return NULL; }
+
+    // if(size <= DSIZE)
+    //     asize = 2*DSIZE;
+    // else
+    //     asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+    // if((bp = find_fit(asize)) != NULL) {
+    //     place(bp, asize);
+    //     return bp;
+    // }
+
+    // extendsize = MAX(asize, CHUNKSIZE);
+    // if((bp = extend_heap(extendsize/WSIZE)) == NULL)
+    //     return NULL;
+
+    // place(bp, asize);
+    // return bp;
 }
+
 
 /*
  * mm_free - Freeing a block does nothing.
@@ -226,7 +352,6 @@ void *mm_realloc(void *ptr, size_t size)
 {
     void *oldptr = ptr;
     void *newptr;
-    size_t copySize;
 
     if(ptr == NULL) { return mm_malloc(size); }
     if(size == 0)
@@ -241,14 +366,6 @@ void *mm_realloc(void *ptr, size_t size)
     memcpy(newptr, oldptr, MIN(GET_SIZE(HDRP(oldptr)), size));
     mm_free(oldptr);
     return newptr;
-    // if (newptr == NULL)
-    //   return NULL;
-    // copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    // if (size < copySize)
-    //   copySize = size;
-    // memcpy(newptr, oldptr, copySize);
-    // mm_free(oldptr);
-    // return newptr;
 }
 
 
